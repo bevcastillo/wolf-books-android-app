869160d7081708869c6eeedb5a4d807af3c03b71
bevcastillo <beverlymaycastillo@gmail.com>
2020-08-14 17:06:02 +0800

    added functionality to check the internet connection

--- app/src/main/java/com/example/bookfinderapp/MainActivity.java
+++ app/src/main/java/com/example/bookfinderapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.bookfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -50,9 +54,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void searchBooks() {
         String queryString = et_search_input.getText().toString();
 
-        Toast.makeText(this, queryString+" is your query", Toast.LENGTH_SHORT).show();
+        //hiding keyboard when search button is clicked
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        //checking the network state
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo !=null && networkInfo.isConnected() && queryString.length() !=0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mTitleText.setText("Loading, please wait");
+            mAuthorText.setText("");
+        }else {
+            if (queryString.length() ==0) {
+                mTitleText.setText("Please enter search term");
+                mAuthorText.setText("");
+            } else {
+                mTitleText.setText("Please cehck your internet connection");
+                mAuthorText.setText("");
+            }
+        }
+
+
+
         Log.i(TAG, "Searched: "+queryString);
         new FetchBook(mTitleText, mAuthorText).execute(queryString);
 
+        if (queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+        } else {
+            Toast.makeText(this, "Please enter a search term.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
