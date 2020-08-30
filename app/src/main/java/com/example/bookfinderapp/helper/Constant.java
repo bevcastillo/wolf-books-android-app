b8f9d979065d9429a4a927b3cbc8022ca92481e4
bevcastillo <beverlymaycastillo@gmail.com>
2020-08-30 14:23:09 +0800

    added splash screen

--- app/src/main/java/com/example/bookfinderapp/helper/Constant.java
+++ app/src/main/java/com/example/bookfinderapp/helper/Constant.java
@@ -6,4 +6,6 @@ public class Constant {
     public static final String BOOK_SEARCH_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
     public static final String BOOK_NEW_URL = "https://www.googleapis.com/books/v1/volumes?orderBy=newest&q=books";
     public static final String BOOK_MAX_RES = "&maxResults=40";
+
+    public static int SPLASH_TIME_OUT = 3000;
 }
